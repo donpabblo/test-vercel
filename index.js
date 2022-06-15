d72f9b1bdfd453adf86e8aae18cdd1b7f2a84e1e
Paolo Servillo <servill7@posteitaliane.it>
2022-06-15 11:58:28 +0200

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const product = require("./api/product");
 const path = require("path");
 
-app.use("/", express.static(path.join(process.cwd(), '/public/')));
+app.use(express.static('public'));
 app.use(express.json({ extended: false }));
 app.use("/api/product", product);
 
